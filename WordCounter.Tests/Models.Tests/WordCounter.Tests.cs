be7c3e03d6c2d58347ab9655b02442bb143e6bfa
Brian Hensley <brnhensley@gmail.com>
2019-04-26 10:06:29 -0700

    successfully fail the second test, to convert strings to alphabet only chars

--- WordCounter.Tests/Models.Tests/WordCounter.Tests.cs
+++ WordCounter.Tests/Models.Tests/WordCounter.Tests.cs
@@ -9,11 +9,25 @@ namespace WordCounter.TestTools
   public class WordCounterTest
   {
     [TestMethod]
-    public void UserInputs_CreateObjectOfWordAndSentece_UserInputs()
+    public void UserInputs_CreateObjectOfWordAndSentece_True()
     {
+      // Arrange
+      // Act
       UserInputs testObject = new UserInputs("jackalope", "Where are my keys");
+      // Assert
       Assert.AreEqual(typeof(UserInputs), testObject.GetType());
     }
 
+    [TestMethod]
+    public void AlphabetOnly_RemoveNonAlphabeticalChars_True()
+    {
+      //Arrange
+      UserInputs testObject = new UserInputs("jackalope", "Where are my keys");
+      string testWord = "GROSS!1!";
+      //Act
+      string newTestWord = testObject.AlphabetOnly(testWord);
+      // Assert
+      Assert.AreEqual(testWord, "GROSS");
+    }
   }
 }
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -12,8 +12,15 @@ namespace WordCounter.Models
     //UserInputs constructor
     public UserInputs(string userWord, string userSentence)
     {
+      //Run both inputs though AlphabetOnly
       _userWord = userWord;
       _userSentence = userSentence;
+    }
+
+    //Removes non-alphabetical chars from the strings & returns them - input is a stand in for both object strings.
+    public string AlphabetOnly(string input)
+    {
+      return input;
     }
 
   }
